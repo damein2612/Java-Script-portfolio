597ab5f2557eb6e17861ea2c76a41c8d14509bf3
damein2612 <dameinwhite@yahoo.com>
2022-09-24 19:01:36 -0700

    update

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,11 @@
+
+function my_Dictionary() {       //this is a key-value pair //
+    var Animal = {
+        Species:"Frog",
+        Color:"Green",
+        Type:"Tree_Frog",
+        Age: 6,
+        Sound:"Peep"
+    };
+    document.getElementById("Dictionary").innerHTML = ;     // statement key id deleted//
+}

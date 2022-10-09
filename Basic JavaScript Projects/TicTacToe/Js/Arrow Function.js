fe6d975a8635f5a908c35f76ec403240a8f8232c
damein2612 <dameinwhite@yahoo.com>
2022-10-09 08:00:43 -0700

    update

---
+++ Basic JavaScript Projects/TicTacToe/Js/Arrow Function.js
@@ -0,0 +1,9 @@
+//function simple() {
+    //var x = "this is a simple javascript";
+    //document.getElementById("Simple").innerHTML = x;
+//}
+
+hello = () => {
+    return "Hello Damein!";
+  }
+ document.getElementById("demo").innerHTML = hello();
